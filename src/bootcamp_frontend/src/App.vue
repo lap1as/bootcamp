4fcc2358c7f624b2521b603d89b6b664e384dfd1
lap1as <kuzukoleg12@gmail.com>
2024-06-24 19:09:07 +0200

    Added counter

--- src/bootcamp_frontend/src/App.vue
+++ src/bootcamp_frontend/src/App.vue
@@ -1,6 +1,23 @@
 <template>
   <main>
     <img src="/logo2.svg" alt="DFINITY logo" />
-    
+  <br/>
+  <button @click="increase">Add</button>
+    {{ licznik }}
   </main>
 </template>
+
+<script>
+export default{
+  data(){
+    return {
+      licznik: 0
+    }
+  },
+  methods: {
+    increase() {
+      this.licznik++
+    }
+  }
+}
+</script>
